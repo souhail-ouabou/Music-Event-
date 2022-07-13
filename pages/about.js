689d5f0f7e41439455408c5c295067a7226b0f26
unknown <souhail7ouabou@gmail.com>
2022-07-13 23:38:40 +0100

    Pages routing

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,10 @@
-export const AboutPage = () => {
-  return (
-    <div>about</div>
-  )
+
+export default function AboutPage() {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>This is an app to find the latest DJ and other musical events</p>
+            <p>Version: 1.0.0</p>
+        </div>
+    )
 }
-            
