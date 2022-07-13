017a412b3b0588d03d66d679a8f654b3254be34a
unknown <souhail7ouabou@gmail.com>
2022-07-14 00:10:23 +0100

    Head Tags

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,12 +1,11 @@
-import { useRouter } from 'next/router'
+import Layout from '../../components/Layout'
 
 export default function EventPage() {
     const router = useRouter()
     console.log(router)
     return (
-        <div>
+        <Layout>
             <h1>My Event</h1>
-            <h3>{router.query.slug}</h3>
-        </div>
+        </Layout>
     )
 }
