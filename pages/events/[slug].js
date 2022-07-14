30d7a585397576b9cef8f4688d4e3e0cefe36239
unknown <souhail7ouabou@gmail.com>
2022-07-14 20:50:02 +0100

    Layout Header and Footer

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,8 +1,7 @@
 import Layout from '../../components/Layout'
 
 export default function EventPage() {
-    const router = useRouter()
-    console.log(router)
+
     return (
         <Layout>
             <h1>My Event</h1>
