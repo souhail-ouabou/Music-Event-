c49512f491c9e5d06936bd3b65637cbdea84ff12
unknown <souhail7ouabou@gmail.com>
2022-07-16 01:22:42 +0100

    Display All Events

--- components/Header.js
+++ components/Header.js
@@ -11,7 +11,7 @@ export default function Header() {
              <nav>
                 <ul>
                     <li>
-                        <Link href='/Events'>
+                        <Link href='/events'>
                             <a>Events</a>
                         </Link>
                     </li>
