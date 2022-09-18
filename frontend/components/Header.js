6900917485ac19fb014a83f0730d0b73a17e9425
unknown <souhail7ouabou@gmail.com>
2022-09-18 12:12:25 +0100

    Add Event Page

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -2,23 +2,28 @@ import Link from 'next/link'
 import Search from './Search'
 import styles from '@/styles/Header.module.css'
 export default function Header() {
-  return (
-    <header className={styles.header}>
-        <div className={styles.logo}>
-             <Link href='/'>
-                <a>DJ Events</a>
-             </Link>
-        </div>
-        <Search/>
-             <nav>
+    return (
+        <header className={styles.header}>
+            <div className={styles.logo}>
+                <Link href="/">
+                    <a>DJ Events</a>
+                </Link>
+            </div>
+            <Search />
+            <nav>
                 <ul>
                     <li>
-                        <Link href='/events'>
+                        <Link href="/events">
                             <a>Events</a>
                         </Link>
                     </li>
+                    <li>
+                        <Link href="/events/add">
+                            <a>Add Event</a>
+                        </Link>
+                    </li>
                 </ul>
-             </nav>
-    </header>
-  )
+            </nav>
+        </header>
+    )
 }
